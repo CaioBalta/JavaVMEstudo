2225c3efe0ae5fd1df7ed42ec0c0d2e26da0b467
CaioBalta <caio.crbaltazar@gmail.com>
2024-09-19 20:43:04 -0300

    Estudo java

---
+++ src/App.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*double [] notas = new double[2];
+        Scanner scan = new Scanner(System.in);
+
+        double somatorio = 0;
+        //String[] nomes = new String[5];
+
+        /*String[] nomes = {"Caio","Balta","Ribero","Cezar", null};*/
+
+        /*for(int c=0; c<=4; c++){
+            System.out.println(nomes[c]);
+        }*/
+
+        /*for(String nome:nomes){
+            System.out.println(nome);
+        };*/
+
+
+        /*VETOR    ^
+         *         | 
+         */
+
+
+         /*int[] idades = new int[3];
+
+         idades[0] = 81;
+         idades[1] = 17;
+         idades[2] = 20;
+
+         System.out.println(idades[2]);
+         idades[0] = 0;
+         System.out.println(idades[0]);/*Foi de F */ 
+
+
+        /*System.out.println("Digite uma nota: ");
+        notas[0] = scan.nextDouble();
+        System.out.println("Digite outra nota");
+        notas[1] = scan.nextDouble();*/
+
+        /*for(int c=0; c<=notas.length; c++){
+            System.out.println("Digite uma nota: ");
+            notas[c] = scan.nextDouble();
+        }
+
+        for( double nota:notas){
+            /*System.out.println(nota);*/
+        /*    somatorio += nota;
+        };
+
+        System.out.println(somatorio/notas.length); */
+
+        /*String [] [] alunos = new String[4][2]; 
+        
+        /*[][] 1- primeiro é linha // 2 - Segundo é coluna  */
+/* 
+
+        alunos[0][0] = "Balta";
+        alunos[0][1] = "10";
+
+        alunos[1][0] = "Caio";
+        alunos[1][1] = "7";
+
+        alunos[2][0] = "Ceza";
+        alunos[2][1] = "8";
+
+        alunos[3][0] = "Ribeir";
+        alunos[3][1] = "2";
+
+        for(String[] linhas:alunos){
+            for(String colunas:linhas){
+                System.out.print(" | " + colunas + " | ");
+            };
+            System.out.println();
+        };*/
+
+        /*String[][] alunoz = {
+            {"Caio ", " 7"},
+            {"Balta ", " 10"}
+        };
+
+
+        alunoz[0][0] = "Cezar";
+        alunoz[0][1] = "9";
+
+        System.out.println(alunoz);*/
+
+        
+    }
+}
